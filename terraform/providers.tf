e1d695b829d52b3afb59994a082e1305ce7a7eec
marlabharghavsai <marlabharghavsai@gmail.com>
2026-02-13 16:06:42 +0000

    Added terraform providers

---
+++ terraform/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3       = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    iam      = "http://localhost:4566"
+  }
+}
